b4deed255a190ca32e044b89738990899429c474
yamakunson <92131933+yamakunson@users.noreply.github.com>
2023-12-11 22:26:17 +0700

    test

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { LoginScreen } from './src/screens/LoginScreen/LoginScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { CalendarScreen } from './src/screens/CalendarScreen/CalendarScreen';
+import { HomeScreen } from './src/screens/HomeScreen/HomeScreen';
+import { LoginScreen } from './src/screens/LoginScreen/LoginScreen';
+import { ProfileScreen } from './src/screens/ProfileScreen/ProfileScreen';
+import { QrCodeScreen } from './src/screens/QrCodeScreen/QrCodeScreen';
+import { SearchScreen } from './src/screens/SearchScreen/SearchScreen';
 import { SignUpScreen } from './src/screens/SignUpScreen/SignUpScreem';
-import { HomeScreen } from './src/screens/QrCodeScreen/QrCodeScreen';
 import { WellcomeScreen } from './src/screens/WellcomeScreen/WellcomeScreen';
 const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Wellcome">
-        <Stack.Screen name="WellCome" component={WellcomeScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="SignUp" component={SignUpScreen} />
+        <Stack.Screen name="Calendar" component={CalendarScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
-
+        <Stack.Screen name="Map" component={LoginScreen} />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+        <Stack.Screen name="QrCode" component={QrCodeScreen} />
+        <Stack.Screen name="Search" component={SearchScreen} />
+        <Stack.Screen name="SignUp" component={SignUpScreen} />
+        <Stack.Screen name="WellCome" component={WellcomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
