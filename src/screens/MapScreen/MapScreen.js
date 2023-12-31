79c17d11eca6d93e78c9a23f646568c2b249bae5
SThanhf <thanh.nguyensinh012@hcmut.edu.vn>
2023-12-31 22:16:06 +0700

    update searchff_bar

--- src/screens/MapScreen/MapScreen.js
+++ src/screens/MapScreen/MapScreen.js
@@ -1,236 +1,100 @@
-// import { Text, View } from "react-native"
-// export const MapScreen = () => {
-//     return (
-//         <View>
-//             <Text>Map Page</Text>
-//         </View>
-//     )
-// }
+//
+// API: AIzaSyBt1drvhF1dGAaxoyW4pMqdxCP_x9WLrjU
+//
 
-// import * as React from "react";
-// import { Image, StyleSheet, View, Text, TextInput, Button, Pressable } from "react-native";
-// import MapView, { Marker } from 'react-native-maps'; //RN maps
-// // import * as Location from 'expo-location'; //expolocation
-// import { fontFamily, Color, FontSize, Border, Padding } from "../../../GlobalStyles";
-// import { useNavigation } from '@react-navigation/native';
-
-// export const MapScreen = () => {
-
-//   const navigation = useNavigation();
-
-//   return (
-//     <View style={{ flex: 1 }}>
-
-//     <MapView
-//         initialRegion={{
-//           latitude: 37.78825,
-//           longitude: -122.4324,
-//           latitudeDelta: 0.0922,
-//           longitudeDelta: 0.0421,
-//         }}
-//       />
-
-//       {/* //nav bar  */}
-
-//       <View style={[styles.navbarbottom, styles.vectorParentShadowBox]}>
-//           <Image
-//             style={styles.vectorIcon1}
-//             resizeMode="cover"
-//             source={require("../../../assets/Home/vector1.png")}
-//           />
-//           <Pressable style={styles.vector} onPress={() => {}}>
-//             <Image
-//               style={styles.iconLayout}
-//               resizeMode="cover"
-//               source={require("../../../assets/Home/vector2.png")}
-//             />
-//           </Pressable>
-//           <Pressable
-//             style={[styles.vector1, styles.union1Layout]}
-//             onPress={() => {}}
-//           >
-//             <Image
-//               style={styles.iconLayout}
-//               resizeMode="cover"
-//               source={require("../../../assets/Home/vector3.png")}
-//             />
-//           </Pressable>
-//           <Pressable
-//             style={[styles.union1, styles.union1Layout]}
-//             onPress={() => {}}
-//           >
-//             <Image
-//               style={styles.iconLayout}
-//               resizeMode="cover"
-//               source={require("../../../assets/Home/union1.png")}
-//             />
-//           </Pressable>
-//         </View>
-
-//       {/* //!nav bar  */}
-
-//     </View>
-//   );
-// };
-
-// //styles
-// const styles = StyleSheet.create({
-//   union1Layout: {
-//     height: 29,
-//     marginLeft: 64,
-//   },
-//   vector1: {
-//     width: 26,
-//     zIndex: 1,
-//   },
-//   union1: {
-//     width: 27,
-//     height: 29,
-//     zIndex: 0,
-//   },
-//   navbarbottom: {
-//     marginLeft: -201.5,
-//     bottom: 0,
-//     borderRadius: Border.br_21xl,
-//     borderColor: Color.colorWhite,
-//     width: 400,
-//     padding: Padding.p_3xs,
-//     backgroundColor: Color.colorWhite,
-//     justifyContent: "center",
-//     alignItems: "flex-end",
-//     borderWidth: 1,
-//     borderStyle: "solid",
-//   },
-// });
-
-// import { StyleSheet, View, Text, Image, Pressable, ScrollView } from "react-native";
-// import { useNavigation } from '@react-navigation/native';
-// import MapView from 'react-native-maps';
-
-// export const MapScreen = () => {
-//     const navigation = useNavigation();
-//     return (
-//   );
-// };
-
-// //styles
-// const styles = StyleSheet.create({
-
-// });
-
-import React from 'react';
-import { StyleSheet, View, Text, Pressable, Image } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-
+import React, { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
+import { useNavigation } from '@react-navigation/native';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 
 export const MapScreen = () => {
   const navigation = useNavigation();
+  const [destination, setDestination] = useState(null);
 
-  // Sample location for the Marker
-  const daihocBachKhoaCoordinates = { latitude: 10.772093966697303, longitude: 106.65790035906763 };
-
+  const handleLocationSelect = (data, details) => {
+    setDestination({
+      latitude: details.geometry.location.lat,
+      longitude: details.geometry.location.lng,
+      title: details.name,
+    });
+  };
 
   return (
     <View style={styles.container}>
-                {/* <Pressable onPress={() => navigation.navigate('Home')}>
-                    <Image
-                        source={require("../../../assets/Home/vector1.png")}
-                        style={styles.iconno1}
-                    />
-                </Pressable>
-                <Pressable onPress={() => { }}>
-                    <Image
-                        source={require("../../../assets/Home/vector2.png")}
-                        style={styles.iconno2}
-                    />
-                </Pressable>
-                <Pressable onPress={() => navigation.navigate('Map')}>
-                    <Image
-                        source={require("../../../assets/Home/vector3.png")}
-                        style={styles.iconno3}
-                    />
-                </Pressable>
-                <Pressable onPress={() => navigation.navigate('Profile')}>
-                    <Image
-                        source={require("../../../assets/Home/union1.png")}
-                        style={styles.iconno3}
-                    />
-                </Pressable> */}
-
-      {/* Header with Back Button */}
-      <View style={styles.header}>
-        <Pressable onPress={() => navigation.goBack()}>
-          <Text style={styles.backButton}>Back</Text>
-        </Pressable>
-        <Text style={styles.headerText}>Map Screen</Text>
-      </View>
-
       {/* Map */}
       <MapView
-  style={styles.map}
-  initialRegion={{
-    latitude: daihocBachKhoaCoordinates.latitude,
-    longitude: daihocBachKhoaCoordinates.longitude,
-    latitudeDelta: 0.0922,
-    longitudeDelta: 0.0421,
-  }}
->
-  {/* Đánh dấu Đại học Bách Khoa trên bản đồ */}
-  <Marker coordinate={daihocBachKhoaCoordinates} title="Đại học Bách Khoa" />
-</MapView>
+        style={styles.map}
+        region={
+          destination
+            ? {
+                latitude: destination.latitude,
+                longitude: destination.longitude,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+              }
+            : {
+                latitude: 10.772093966697303, 
+                longitude: 106.65790035906763,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+              }
+        }
+      >
+        {/* Selected Destination Marker */}
+        {destination && (
+          <Marker
+            coordinate={{
+              latitude: destination.latitude,
+              longitude: destination.longitude,
+            }}
+            title={destination.title}
+          />
+        )}
+      </MapView>
+
+      {/* Search Bar */}
+      <GooglePlacesAutocomplete
+        placeholder="Nhập địa điểm"
+        onPress={handleLocationSelect}
+        query={{
+          key: "AIzaSyBt1drvhF1dGAaxoyW4pMqdxCP_x9WLrjU",
+          language: 'en',
+        }}
+        styles={{
+          container: {
+            position: 'absolute',
+            top: 10,
+            left: 10,
+            right: 10,
+          },
+          textInputContainer: {
+            width: '100%',
+            backgroundColor: 'rgba(0,0,0,0)',
+            borderTopWidth: 0,
+            borderBottomWidth: 0,
+          },
+          textInput: {
+            marginLeft: 0,
+            marginRight: 0,
+            height: 38,
+            color: '#5d5d5d',
+            fontSize: 16,
+          },
+          predefinedPlacesDescription: {
+            color: '#1faadb',
+          },
+        }}
+      />
     </View>
-    
   );
 };
 
-// Styles
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  header: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    padding: 10,
-    backgroundColor: '#3498db', // Example color
-  },
-  backButton: {
-    color: 'white',
-    fontWeight: 'bold',
-  },
-  headerText: {
-    color: 'white',
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
   map: {
     flex: 1,
   },
-//   container: {
-//     position: 'absolute',
-//     bottom: 5,
-//     flexDirection: 'row',
-//     justifyContent: 'space-around',
-//     alignItems: 'center',
-//     backgroundColor: 'white',
-//     height: 60,
-//     paddingHorizontal: 20,
-//     width: '100%',
-//     borderRadius: 20,
-//   },
-//   iconno1: {
-//     width: 34,
-//     height: 24,
-// },
-//   iconno2: {
-//     width: 27,
-//     height: 30,
-// },
-//   iconno3: {
-//     width: 27,
-//     height: 29,
-// },  
 });
-
