7be8abe640fd26bf27a6d62b261b222f7cdd2e5a
tuananh0303 <anhhello564@gmail.com>
2024-01-02 20:41:41 +0700

    updated

--- src/screens/SignUpScreen/SignUpScreem.js
+++ src/screens/SignUpScreen/SignUpScreem.js
@@ -1,12 +1,20 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Pressable,
+  Image,
+} from "react-native";
 import { Input, Button, Icon, NativeBaseProvider } from "native-base";
 import { FontAwesome5, MaterialCommunityIcons } from "@expo/vector-icons";
-import { useNavigation } from "@react-navigation/native";
+import { useFocusEffect, useNavigation } from "@react-navigation/native";
 import { useMutationHooks } from "../../hooks/useMutationHook";
 import * as UserService from "../../services/UserServices";
 import ENV_VARS from "../../../env";
+import { LinearGradient } from "expo-linear-gradient";
 
 const apiURL = ENV_VARS.API_URL;
 
@@ -39,7 +47,6 @@ function SignUpScreen() {
     console.log(mutation);
     console.log("data:", data);
   }, [isSuccess]);
-
   const handleSignUp = () => {
     console.log("Username:", username);
     console.log("Email:", email);
@@ -54,9 +61,33 @@ function SignUpScreen() {
   };
   return (
     <View style={styles.container}>
-      <View style={styles.Middle}>
+      {/* <View style={styles.Middle}>
         <Text style={styles.LoginText}>Tạo tài khoản</Text>
         <Text>Đăng ký tài khoản tại đây để sử dụng ứng dụng</Text>
+      </View> */}
+      <View style={{ marginTop: 30 }}>
+        <LinearGradient
+          colors={["#87CBF0", "#6D9BF5"]}
+          start={[0, 0]}
+          end={[1, 0]}
+          style={styles.gradient}
+        >
+          <Pressable
+            style={[styles.backIconCon]}
+            onPress={() => navigation.navigate("Wellcome1")}
+          >
+            <Image
+              source={require("../../../assets/Profile1/frame-18.png")}
+              style={styles.backIcon}
+            />
+          </Pressable>
+          <View style={styles.Middle}>
+            <Text style={styles.LoginText}>Tạo tài khoản</Text>
+            <Text style={{ color: "#686868" }}>
+              Đăng ký tài khoản tại đây để sử dụng ứng dụng
+            </Text>
+          </View>
+        </LinearGradient>
       </View>
 
       {/* Username or Email Input Field */}
@@ -152,14 +183,22 @@ function SignUpScreen() {
           />
         </View>
       </View>
-      {data?.status === "ERR" && (
-        <Text style={{ color: "red" }}>{data?.message}</Text>
+      {data && data?.status === "ERR" && (
+        <Text
+          style={{ color: "red", position: "absolute", top: 405, left: 15 }}
+        >
+          {data?.message}
+        </Text>
       )}
-      {/* Button */}
-      <View style={styles.buttonStyle}>
-        <Button style={styles.buttonDesign} onPress={handleSignUp}>
-          Đăng ký
-        </Button>
+      {/* Button signup */}
+      <View style={styles.buttonWrapper}>
+        <LinearGradient
+          colors={["#9C3FE4", "#C65647"]}
+          style={[StyleSheet.absoluteFill, styles.gradient1]}
+        />
+        <TouchableOpacity style={styles.buttonLogin} onPress={handleSignUp}>
+          <Text style={styles.buttonText}>Đăng ký</Text>
+        </TouchableOpacity>
       </View>
 
       {/* Line */}
@@ -171,60 +210,41 @@ function SignUpScreen() {
         <View style={{ flex: 1, height: 1, backgroundColor: "black" }} />
       </View>
 
-      {/* Box */}
-      <View style={styles.buttonStyle}>
-        <Button
-          onPress={() => navigation.navigate("#")} // for navigation
-          style={styles.buttonDesign1}
-          shadow={3}
-          _light={{
-            backgroundColor: "gray.50",
-          }}
-          _dark={{
-            backgroundColor: "gray.700",
-          }}
-        >
-          <Icon
-            as={<FontAwesome5 name="google" />}
-            size="sm"
-            m={2}
-            _light={{
-              color: "black",
-            }}
-            _dark={{
-              color: "gray.300",
-            }}
-          />
-        </Button>
-        <Button
-          onPress={() => navigation.navigate("#")} // for navigation
-          style={styles.buttonDesign}
-          shadow={3}
-          _light={{
-            backgroundColor: "gray.50",
-          }}
-          _dark={{
-            backgroundColor: "gray.700",
-          }}
-        >
-          <Icon
-            as={<FontAwesome5 name="facebook" />}
-            size="sm"
-            m={2}
-            _light={{
-              color: "black",
-            }}
-            _dark={{
-              color: "gray.300",
-            }}
-          />
-        </Button>
+      <View style={{ marginTop: 20, flexDirection: "column", gap: 10 }}>
+        <View style={styles.buttonWrapper1}>
+          <TouchableOpacity
+            style={styles.buttonLogin1}
+            onPress={() => navigation.navigate("Login")}
+          >
+            <FontAwesome5
+              name="google"
+              size={21}
+              color="red" // Đặt màu cho icon ở đây
+              style={styles.iconStyle}
+            />
+            <Text style={styles.buttonText1}>Google</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.buttonWrapper1}>
+          <TouchableOpacity
+            style={styles.buttonLogin1}
+            onPress={() => navigation.navigate("Login")}
+          >
+            <FontAwesome5
+              name="facebook"
+              size={21}
+              color="blue" // Đặt màu cho icon ở đây
+              style={styles.iconStyle}
+            />
+            <Text style={styles.buttonText1}>Facebook</Text>
+          </TouchableOpacity>
+        </View>
       </View>
 
       <View style={styles.text2}>
-        <Text>Bạn đã có tài khoản? </Text>
+        <Text style={{ fontSize: 17 }}>Bạn chưa có tài khoản? </Text>
         <TouchableOpacity onPress={() => navigation.navigate("Login")}>
-          <Text style={styles.signupText}> Đăng nhập </Text>
+          <Text style={styles.signupText}> Đăng nhập</Text>
         </TouchableOpacity>
       </View>
 
@@ -242,25 +262,28 @@ export default () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#87B7E7",
   },
   LoginText: {
-    marginTop: 40,
-    marginBottom: 10,
-    fontSize: 30,
+    marginBottom: 7,
+    fontSize: 35,
     fontWeight: "bold",
   },
   Middle: {
+    paddingTop: 5,
+    paddingBottom: 25,
     alignItems: "center",
     justifyContent: "center",
   },
   text2: {
     flexDirection: "row",
     justifyContent: "center",
-    paddingTop: 5,
+    marginTop: 35,
+    backgroundColor: "#698ED7",
   },
   signupText: {
     fontWeight: "bold",
+    fontSize: 17,
   },
   emailField: {
     marginTop: 30,
@@ -269,16 +292,75 @@ const styles = StyleSheet.create({
   emailInput: {
     marginTop: 10,
     marginRight: 5,
+    backgroundColor: "#FFFFFF",
+    borderRadius: 10,
   },
   buttonStyle: {
-    marginTop: 30,
+    marginTop: 25,
     marginLeft: 15,
     marginRight: 15,
+  },
+  backIcon: {
+    width: 35,
+    height: 35,
+    marginRight: 20,
+  },
+  backIconCon: {
+    marginLeft: 20,
+    marginTop: 15,
   },
   buttonStyleX: {
     marginTop: 12,
     marginLeft: 15,
     marginRight: 15,
+  },
+  buttonWrapper: {
+    marginTop: 40,
+    marginLeft: 15,
+    marginRight: 15,
+    borderRadius: 10,
+    overflow: "hidden", // Để đảm bảo gradient không tràn ra ngoài
+  },
+  buttonWrapper1: {
+    marginLeft: 15,
+    marginRight: 15,
+    borderRadius: 10,
+    overflow: "hidden", // Để đảm bảo gradient không tràn ra ngoài
+  },
+  buttonLogin: {
+    backgroundColor: "transparent",
+    borderRadius: 10,
+    paddingVertical: 10,
+    // paddingHorizontal: 20,
+  },
+  buttonLogin1: {
+    backgroundColor: "rgba(0, 0, 0, 0.9)",
+    borderRadius: 10,
+    paddingVertical: 7,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 7,
+    // paddingHorizontal: 20,
+  },
+  buttonText: {
+    textAlign: "center",
+    color: "#FFF",
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+  buttonText1: {
+    textAlign: "center",
+    color: "#FFF",
+    fontWeight: "bold",
+    fontSize: 19,
+  },
+  gradient1: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
   },
   buttonDesign: {
     backgroundColor: "#026efd",
@@ -289,7 +371,7 @@ const styles = StyleSheet.create({
   },
   lineStyle: {
     flexDirection: "row",
-    marginTop: 30,
+    marginTop: 20,
     marginLeft: 15,
     marginRight: 15,
     alignItems: "center",
