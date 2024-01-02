6f53347d56404cb6e0a9797b3abe685cce1368e4
hung1306 <mycode13062002@gmail.com>
2024-01-02 08:55:08 +0700

    wip

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-// <<<<<<< HEAD
 import { StyleSheet, Text, View, AppRegistry } from "react-native";
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -6,7 +5,6 @@ import { ApplicationProvider } from '@ui-kitten/components';
 import * as eva from '@eva-design/eva';
 import  CalendarScreen  from './src/screens/CalendarScreen/CalendarScreen';
 import { HomeScreen } from './src/screens/HomeScreen/HomeScreen';
-import { LoginScreen } from './src/screens/LoginScreen/LoginScreen';
 import { MapScreen } from './src/screens/MapScreen/MapScreen';
 import { ProfileScreen } from './src/screens/ProfileScreen/ProfileScreen';
 import { ChangeInfoScreen } from './src/screens/ProfileScreen/ChangeInfo';
@@ -14,7 +12,7 @@ import { InfoScreen } from './src/screens/ProfileScreen/Info';
 import { ChangePassScreen } from './src/screens/ProfileScreen/ChangePass';
 import { ChangeAvatarScreen } from './src/screens/ProfileScreen/ChangeAvatar';
 import { SearchScreen } from './src/screens/SearchScreen/SearchScreen';
-import { SignUpScreen } from './src/screens/SignUpScreen/SignUpScreem';
+import  SignUpScreen  from './src/screens/SignUpScreen/SignUpScreem';
 import { WellcomeScreen } from './src/screens/WellcomeScreen/WellcomeScreen';
 import React, { useState } from 'react';
 import { InfoSearchScreen } from './src/screens/SearchScreen/InfoSearchScreen';
@@ -24,27 +22,9 @@ import CalendarScreen2 from './src/screens/CalendarScreen/CalendarScreen2'
 import CalendarScreen3 from './src/screens/CalendarScreen/CalendarScreen3'
 import { InfoSearchScreen2 } from './src/screens/SearchScreen/InfoSearchScreen2';
 import { InfoSearchScreen3 } from './src/screens/SearchScreen/InfoSearchScreen3';
-=======
-import { StatusBar } from "expo-status-bar";
-// import { NavigationContainer } from "@react-navigation/native";
-// import { createStackNavigator } from "@react-navigation/stack";
-// import { CalendarScreen } from "./src/screens/CalendarScreen/CalendarScreen";
-// import { HomeScreen } from "./src/screens/HomeScreen/HomeScreen";
-// import LoginScreen from "./src/screens/LoginScreen/LoginScreen";
-// import { ProfileScreen } from "./src/screens/ProfileScreen/ProfileScreen";
-// import { ChangeInfoScreen } from "./src/screens/ProfileScreen/ChangeInfo";
-// import { InfoScreen } from "./src/screens/ProfileScreen/Info";
-// import { ChangePassScreen } from "./src/screens/ProfileScreen/ChangePass";
-// import { ChangeAvatarScreen } from "./src/screens/ProfileScreen/ChangeAvatar";
-// import { QrCodeScreen } from "./src/screens/QrCodeScreen/QrCodeScreen";
-// import { SearchScreen } from "./src/screens/SearchScreen/SearchScreen";
-// import SignUpScreen from "./src/screens/SignUpScreen/SignUpScreem";
-// import { WellcomeScreen } from "./src/screens/WellcomeScreen/WellcomeScreen";
-// import { WellcomeScreen1 } from "./src/screens/WellcomScreen1/WellcomScreen1";
-// import { InfoSearchScreen } from "./src/screens/SearchScreen/InfoSearchScreen";
+import { WellcomeScreen1 } from "./src/screens/WellcomScreen1/WellcomScreen1";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-// >>>>>>> 4bcc54b96ee96829738155a43934d749424388f6
-
+import LoginScreen from "./src/screens/LoginScreen/LoginScreen";
 const Stack = createStackNavigator();
 const queryClient = new QueryClient();
 
@@ -78,7 +58,6 @@ export default function App() {
 //       </Stack.Navigator>
 //     </NavigationContainer>
 //     </ApplicationProvider>
-// =======
   return (
     <QueryClientProvider client={queryClient}>
       <NavigationContainer>
@@ -89,7 +68,7 @@ export default function App() {
           <Stack.Screen name="Home" component={HomeScreen} />
           <Stack.Screen name="Login" component={LoginScreen} />
           <Stack.Screen name="Calendar" component={CalendarScreen} />
-          <Stack.Screen name="Map" component={LoginScreen} />
+          <Stack.Screen name="Map" component={MapScreen} />
           <Stack.Screen name="Profile" component={ProfileScreen} />
           <Stack.Screen name="ChangeInfo" component={ChangeInfoScreen} />
           <Stack.Screen name="Info" component={InfoScreen} />
@@ -104,13 +83,11 @@ export default function App() {
         </Stack.Navigator>
       </NavigationContainer>
     </QueryClientProvider>
-// >>>>>>> 4bcc54b96ee96829738155a43934d749424388f6
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    fontFamily: "OpenSans-SemiBoldItalic",
     flex: 1,
     backgroundColor: "#fafafa",
     alignItems: "center",
